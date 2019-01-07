82df03acd03623876f6ad0573b022b193a1d7f86
Bosco Lyu <hite95@gmail.com>
2019-01-08 00:59:38 +0900

    Go lang programming language chapter1

---
+++ go/src/org/book/chapter1/ex1_3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	s, sep := "", ""
+	fmt.Println(time.Now())
+	count := strconv.Itoa(os.Args[1])
+
+	for {
+		s = sep + count
+		if count > 100 {
+			break;
+		}
+	}
+	fmt.Println(s)
+	fmt.Println(time.Now())
+
+
+	fmt.Println(strings.Join(os.Args[2], " "))
+	fmt.Println(time.Now())
+	fmt.Println(os.Args)
+	fmt.Println(time.Now())
+}
